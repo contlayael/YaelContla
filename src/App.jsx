a34016f6073bb86ee5ff034be17a91838f9e2b20
yaelcontla <yael.contla@hotmail.com>
2025-11-18 17:58:37 -0600

    social medias footer added

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       id: 1,
       title: "Catálogo de productos",
       icon: <FaTshirt />,
-      url: "https://tutienda.com", // Pon aquí tu link real
+      url: "https://wa.me/message/CKTR6O3NBP2SN1", // Pon aquí tu link real
     },
     {
       id: 2,
@@ -78,10 +78,10 @@ function App() {
       <footer className="social-footer">
         <p className="footer-text">Aquí resolvemos tus dudas</p>
         <div className="social-icons">
-          <a href="#"><FaInstagram /></a>
-          <a href="#"><FaYoutube /></a>
-          <a href="#"><FaFacebook /></a>
-          <a href="#"><FaTiktok /></a>
+          <a href="https://www.instagram.com/yaelcontla?igsh=MWhzN3ZveGhjdWNoNw==" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+          <a href="https://youtube.com/@yaelcontla?si=WTlSE6Aaops7VAc2" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
+          <a href="https://www.facebook.com/share/14NsYtDT5V9/" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+          <a href="https://www.tiktok.com/@yaelcontla15?_r=1&_t=ZS-91VyltuoLah" target="_blank" rel="noopener noreferrer"><FaTiktok /></a>
         </div>
         <p className="copyright">© Yael Contla {new Date().getFullYear()}</p>
       </footer>
